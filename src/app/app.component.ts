62b8a95b667290e8a2424f2f604b4bc06c7ef766
Egzon Uka <egzonuka8@gmail.com>
2023-03-07 14:17:35 +0100

    feat: created compoentn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'boocamp-components';
+  posts = [
+    {
+      title: 'title one',
+    },
+    {
+      title: 'title two',
+    },
+    {
+      title: 'title three',
+    },
+  ];
 }
