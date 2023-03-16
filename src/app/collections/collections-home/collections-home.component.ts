fa0606e7d3189d990254e618c2f9b34c860ab964
Egzon Uka <egzonuka8@gmail.com>
2023-03-16 15:45:38 +0100

    feat: added new component for table, using template directives to show the content inside the table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent {
-
+  data: any = [
+    {
+      name: 'Egzon',
+      age: 30,
+      job: 'programer',
+    },
+    {
+      name: 'Tomi',
+      age: 33,
+      job: 'designer',
+    },
+    {
+      name: 'Xonn',
+      age: 20,
+      job: 'AI',
+    },
+  ];
+  headers: any = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 }
